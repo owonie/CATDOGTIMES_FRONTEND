8c524c44e459abe65e858955dea61b7fb051e543
Signalzero <chlehdnjs@gmail.com>
2022-12-27 15:54:09 +0900

    rename(WalkMap.jsx): rename

---
+++ src/components/WalkMap/WalkMap.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react';
+import styles from './WalkMap.module.css';
+
+const { kakao } = window;
+
+const WalkMap = (props) => {
+  useEffect(() => {
+    const container = document.getElementById('map');
+    const options = {
+      center: new kakao.maps.LatLng(33.450701, 126.570667),
+      level: 3,
+    };
+    const map = new kakao.maps.Map(container, options);
+  }, []);
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.leftTab}>leftTab</div>
+        <div className={styles.mainTab}>
+          <div id='map' className={styles.walkMap}></div>
+        </div>
+        <div className={styles.rightTab}>rightTab</div>
+      </div>
+    </>
+  );
+};
+
+export default WalkMap;
