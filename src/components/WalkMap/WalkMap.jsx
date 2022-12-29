20bb80bf9db97cbe36ec6c037a70464d3d3835b8
Signalzero <chlehdnjs@gmail.com>
2022-12-29 23:21:50 +0900

    feat: search place marking

--- src/components/WalkMap/WalkMap.jsx
+++ src/components/WalkMap/WalkMap.jsx
@@ -3,127 +3,54 @@ import styles from './WalkMap.module.css';
 
 const { kakao } = window;
 
-const WalkMap = (props) => {
-  const [searchData, setSearchData] = useState();
-  const [markerPositions, setMarkerPositions] = useState([]);
-  const searchRef = useRef();
-  const [tabState, setTabState] = useState('');
-
-  const handleTabStateChanged = (e) => {
-    setTabState(e);
-  };
-  const handleSearchInput = () => {
-    setSearchData(searchRef.current.value);
-    console.log(searchRef.current.value);
-  };
-
+const WalkMap = ({ searchPlace }) => {
+  const [Places, setPlaces] = useState([]);
   useEffect(() => {
-    const mapContainer = document.getElementById('map');
+    var infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
+
+    const container = document.getElementById('map');
     const options = {
       center: new kakao.maps.LatLng(33.450701, 126.570667),
       level: 3,
     };
-    const map = new kakao.maps.Map(mapContainer, options);
-  }, []);
+    const map = new kakao.maps.Map(container, options);
+    const ps = new kakao.maps.services.Places();
 
-  return (
-    <>
-      <div className={styles.container}>
-        <div className={styles.walkNavbar}>
-          <div>지도 네이게이션바</div>
-          <div className={styles.navbarHeader}>
-            <div className={styles.searchBarHeader}>
-              <button className={styles.burgerButton}>햄버거</button>
-              <button className={styles.homeButton}>멍냥일보</button>
-            </div>
-            <div className={styles.searchBar}>
-              <div className={styles.searchBox}>
-                <input
-                  id='ID'
-                  data-testid='input-box'
-                  placeholder='장소, 주소, 버스 검색'
-                  type='text'
-                  className={styles.searchInput}
-                  ref={searchRef}
-                />
-              </div>
-              <button
-                className={styles.searchButton}
-                onClick={handleSearchInput}
-              >
-                검색버튼
-              </button>
-            </div>
-            <div className={styles.navTabButton}>
-              <button onClick={() => handleTabStateChanged('검색')}>
-                검색
-              </button>
-              <button onClick={() => handleTabStateChanged('루트')}>
-                루트
-              </button>
-              <button onClick={() => handleTabStateChanged('MY')}>MY</button>
-            </div>
-          </div>
-          <div className={styles.navbarBody}>
-            <div
-              style={tabState === '검색' ? null : { display: 'none' }}
-              className={styles.navigateTab}
-            >
-              <div className={styles.infoWeather}>이거슨 날씨여</div>
-              <div className={styles.infoAround}>
-                <h3>주변 탐색</h3>
-                <ul className={styles.infoAroundButtons}>
-                  <li className={styles.infoAroundList}>
-                    <button className={styles.infoAroundButton}>
-                      <span className={styles.infoAroundText}>공원</span>
-                    </button>
-                  </li>
-                  <li className={styles.infoAroundList}>
-                    <button className={styles.infoAroundButton}>
-                      <span className={styles.infoAroundText}>반려카페</span>
-                    </button>
-                  </li>
-                  <li className={styles.infoAroundList}>
-                    <button className={styles.infoAroundButton}>
-                      <span className={styles.infoAroundText}>동물병원</span>
-                    </button>
-                  </li>
-                  <li className={styles.infoAroundList}>
-                    <button className={styles.infoAroundButton}>
-                      <span className={styles.infoAroundText}>펫 샵</span>
-                    </button>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div
-              style={tabState === '루트' ? null : { display: 'none' }}
-              className={styles.directionsTab}
-            >
-              이건 길찾기
-              <div className={styles.infoRouteSearchBox}>
-                <div className={styles.infoRouteList}>
-                  <div className={styles.infoRouteStart}>출발지</div>
-                  <div className={styles.infoRouteVia}>경유지</div>
-                  <div className={styles.infoRouteEnd}>도착지</div>
-                </div>
-              </div>
-            </div>
-            <div
-              style={tabState === 'MY' ? null : { display: 'none' }}
-              className={styles.myRouteTab}
-            >
-              이건 마이루트
-            </div>
-          </div>
-        </div>
+    const placesSearchCB = (data, status) => {
+      if (status === kakao.maps.services.Status.OK) {
+        let bounds = new kakao.maps.LatLngBounds();
 
-        <div className={styles.mapWrapper}>
-          <div id='map' className={styles.walkMap}></div>
-        </div>
-      </div>
-    </>
-  );
+        for (let i = 0; i < data.length; i++) {
+          displayMarker(data[i]);
+          bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+        }
+
+        map.setBounds(bounds);
+        // 페이지 목록 보여주는 displayPagination() 추가
+
+        setPlaces(data);
+      }
+    };
+
+    const displayMarker = (place) => {
+      let marker = new kakao.maps.Marker({
+        map: map,
+        position: new kakao.maps.LatLng(place.y, place.x),
+      });
+
+      kakao.maps.event.addListener(marker, 'click', function () {
+        infowindow.setContent(
+          '<div style="padding:5px;font-size:12px;">' +
+            place.place_name +
+            '</div>'
+        );
+        infowindow.open(map, marker);
+      });
+    };
+
+    ps.keywordSearch(searchPlace, placesSearchCB);
+  }, [searchPlace]);
+
+  return <div id='map' className={styles.walkMap}></div>;
 };
-
 export default WalkMap;
--- src/routes/Walk/Walk.jsx
+++ src/routes/Walk/Walk.jsx
@@ -1,14 +1,113 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import WalkMap from '../../components/WalkMap/WalkMap';
 import styles from './Walk.module.css';
 
 const Walk = (props) => {
+  const [searchPlace, setSearchPlace] = useState();
+  const [markerPositions, setMarkerPositions] = useState([]);
+  const searchRef = useRef();
+  const [tabState, setTabState] = useState('');
+
+  const handleTabStateChanged = (e) => {
+    setTabState(e);
+  };
+  const handleSearchInput = () => {
+    setSearchPlace(searchRef.current.value);
+  };
   return (
     <>
       <header>walk page</header>
       <div className={styles.container}>
+        <div className={styles.walkNavbar}>
+          <div>지도 네이게이션바</div>
+          <div className={styles.navbarHeader}>
+            <div className={styles.searchBarHeader}>
+              <button className={styles.burgerButton}>햄버거</button>
+              <button className={styles.homeButton}>멍냥일보</button>
+            </div>
+            <div className={styles.searchBar}>
+              <div className={styles.searchBox}>
+                <input
+                  id='ID'
+                  data-testid='input-box'
+                  placeholder='장소, 주소, 버스 검색'
+                  type='text'
+                  className={styles.searchInput}
+                  ref={searchRef}
+                />
+              </div>
+              <button
+                className={styles.searchButton}
+                onClick={handleSearchInput}
+              >
+                검색버튼
+              </button>
+            </div>
+            <div className={styles.navTabButton}>
+              <button onClick={() => handleTabStateChanged('검색')}>
+                검색
+              </button>
+              <button onClick={() => handleTabStateChanged('루트')}>
+                루트
+              </button>
+              <button onClick={() => handleTabStateChanged('MY')}>MY</button>
+            </div>
+          </div>
+          <div className={styles.navbarBody}>
+            <div
+              style={tabState === '검색' ? null : { display: 'none' }}
+              className={styles.navigateTab}
+            >
+              <div className={styles.infoWeather}>이거슨 날씨여</div>
+              <div className={styles.infoAround}>
+                <h3>주변 탐색</h3>
+                <ul className={styles.infoAroundButtons}>
+                  <li className={styles.infoAroundList}>
+                    <button className={styles.infoAroundButton}>
+                      <span className={styles.infoAroundText}>공원</span>
+                    </button>
+                  </li>
+                  <li className={styles.infoAroundList}>
+                    <button className={styles.infoAroundButton}>
+                      <span className={styles.infoAroundText}>반려카페</span>
+                    </button>
+                  </li>
+                  <li className={styles.infoAroundList}>
+                    <button className={styles.infoAroundButton}>
+                      <span className={styles.infoAroundText}>동물병원</span>
+                    </button>
+                  </li>
+                  <li className={styles.infoAroundList}>
+                    <button className={styles.infoAroundButton}>
+                      <span className={styles.infoAroundText}>펫 샵</span>
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div
+              style={tabState === '루트' ? null : { display: 'none' }}
+              className={styles.directionsTab}
+            >
+              이건 길찾기
+              <div className={styles.infoRouteSearchBox}>
+                <div className={styles.infoRouteList}>
+                  <div className={styles.infoRouteStart}>출발지</div>
+                  <div className={styles.infoRouteVia}>경유지</div>
+                  <div className={styles.infoRouteEnd}>도착지</div>
+                </div>
+              </div>
+            </div>
+            <div
+              style={tabState === 'MY' ? null : { display: 'none' }}
+              className={styles.myRouteTab}
+            >
+              이건 마이루트
+            </div>
+          </div>
+        </div>
         <div className={styles.walkMap}>
-          <WalkMap />
+          <WalkMap searchPlace={searchPlace} />
         </div>
       </div>
     </>
