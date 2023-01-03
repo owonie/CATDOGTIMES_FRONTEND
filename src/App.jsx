a5930ec41ca4ef32b6fd0110584372ffae417932
Signalzero <chlehdnjs@gmail.com>
2023-01-03 19:54:38 +0900

    feat:(App): add weather api key

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,18 @@
-import { useEffect, useState } from 'react';
 import styles from './app.module.css';
 import { Routes, Route } from 'react-router-dom';
 import Walk from './routes/Walk/Walk';
 import Login from './routes/Login/Login';
 
 const App = () => {
-  const [message, setMessage] = useState();
-  useEffect(() => {
-    fetch('/')
-      .then((res) => {
-        return res.status;
-      })
-      .then((data) => {
-        if (data === 200) {
-          setMessage('백엔드와 연결 성공!');
-        }
-      });
-  }, []);
+  const weatherKey = process.env.REACT_APP_WEATHER_API_KEY;
   return (
     <>
       <div className='App'>
-        <div>멍냥일보 프론트엔드입니당 {message}</div>
         <Routes>
-          <Route path='/walk' element={<Walk />}></Route>
+          <Route
+            path='/walk'
+            element={<Walk weatherKey={weatherKey} />}
+          ></Route>
           <Route path='/login' element={<Login />}></Route>
         </Routes>
       </div>
