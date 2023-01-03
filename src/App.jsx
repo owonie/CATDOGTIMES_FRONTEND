34290a72d9d8f81d18b52d34bed5fab7690c4304
Signalzero <chlehdnjs@gmail.com>
2023-01-03 20:02:56 +0900

    fix(login): delete login component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import styles from './app.module.css';
 import { Routes, Route } from 'react-router-dom';
 import Walk from './routes/Walk/Walk';
-import Login from './routes/Login/Login';
 
 const App = () => {
   const weatherKey = process.env.REACT_APP_WEATHER_API_KEY;
@@ -13,7 +12,6 @@ const App = () => {
             path='/walk'
             element={<Walk weatherKey={weatherKey} />}
           ></Route>
-          <Route path='/login' element={<Login />}></Route>
         </Routes>
       </div>
     </>
