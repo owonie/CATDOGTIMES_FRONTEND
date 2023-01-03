735f392cedf92fe3d63f9c984a63fa1bf827f410
SHINPC\jin <shinkookjin2@gmail.com>
2023-01-03 23:04:53 +0900

    index.html App.jsx  충돌난 부분 수작업 해결함.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import styles from './app.module.css';
 import { Routes, Route, Link } from 'react-router-dom';
 import Walk from './routes/Walk/Walk';
+import SNS from "./routes/SNS/SNS";
+import Explore from "./routes/Explore/Explore";
 import Mypage from './routes/Mypage/Mypage';
 import Testlogin from './routes/Mypage/Testlogin';
-
 
 import Mypage_updatemyinfo from './routes/Mypage/Mypage_updatemyinfo';
 
 const App = () => {
-  const [message, setMessage] = useState();
-  useEffect(() => {
-    fetch('/')
-      .then((res) => {
-        return res.status;
-      })
-      .then((data) => {
-        if (data === 200) {
-          setMessage('백엔드와 연결 성공!');
-        }
-      });
-  }, []);
-
-
+  const weatherKey = process.env.REACT_APP_WEATHER_API_KEY;
   const [user, setUser] = useState(null);
+
   const sessionInfo =(user)=>{
     console.log("--------");
     console.log(user);
     setUser(user);
   }
-
 
   return (
     <>
@@ -42,15 +30,14 @@ const App = () => {
           <Link to="/mypageupdate" className='button'> 정보수정</Link>
         </div>
         <Routes>
-          <Route path='/walk' element={<Walk />}></Route>
+          <Route path="/walk" element={<Walk weatherKey={weatherKey} />}></Route>
+          <Route path="/post" element={<SNS />}></Route>
+          <Route path="/explore" element={<Explore />}></Route>
           
           <Route path='/testlogin' element={<Testlogin />}></Route>
           <Route path='/memberinfo' element={<Mypage user={user} sessionInfo={sessionInfo} />}></Route>
           <Route path='/mypageupdate' element={<Mypage_updatemyinfo user={user} sessionInfo={sessionInfo} />}></Route>
         </Routes>
-
-        
-
       </div>
     </>
   );
