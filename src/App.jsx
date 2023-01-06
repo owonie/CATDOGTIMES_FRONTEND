2096bbd3c6770f93c5db5f647be71a7725ff910c
Signalzero <chlehdnjs@gmail.com>
2023-01-06 13:57:58 +0900

    feat: feat redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useEffect, useState } from 'react';
 import styles from './app.module.css';
 import { Routes, Route, Link } from 'react-router-dom';
 import Walk from './routes/Walk/Walk';
+import SNS from './routes/SNS/SNS';
 import Mypage from './routes/Mypage/Mypage';
 import Testlogin from './routes/Mypage/Testlogin';
-
-
+import Explore from './routes/Explore/Explore';
 import Mypage_updatemyinfo from './routes/Mypage/Mypage_updatemyinfo';
 
 const App = () => {
+  const weatherKey = process.env.REACT_APP_WEATHER_API_KEY;
   const [message, setMessage] = useState();
   useEffect(() => {
     fetch('/')
@@ -22,35 +23,49 @@ const App = () => {
       });
   }, []);
 
-
   const [user, setUser] = useState(null);
-  const sessionInfo =(user)=>{
-    console.log("--------");
+  const sessionInfo = (user) => {
+    console.log('--------');
     console.log(user);
     setUser(user);
-  }
-
+  };
 
   return (
     <>
       <div className='App'>
         <div className='text-center'>
-          <div>멍냥일보 프론트엔드입니당 {message}</div>
-          
-          <Link to="/testlogin" className='button'>테스트 로그인</Link>
-          <Link to="/memberinfo" className='button'>멤버인포</Link>
-          <Link to="/mypageupdate" className='button'> 정보수정</Link>
+          <Link to='/testlogin' className='button'>
+            테스트 로그인
+          </Link>
+          <Link to='/memberinfo' className='button'>
+            멤버인포
+          </Link>
+          <Link to='/mypageupdate' className='button'>
+            {' '}
+            정보수정
+          </Link>
         </div>
         <Routes>
-          <Route path='/walk' element={<Walk />}></Route>
-          
+          <Route
+            path='/walk'
+            element={<Walk weatherKey={weatherKey} />}
+          ></Route>
+
+          <Route path='/post' element={<SNS />}></Route>
+          <Route path='/explore' element={<Explore />}></Route>
+
           <Route path='/testlogin' element={<Testlogin />}></Route>
-          <Route path='/memberinfo' element={<Mypage user={user} sessionInfo={sessionInfo} />}></Route>
-          <Route path='/mypageupdate' element={<Mypage_updatemyinfo user={user} sessionInfo={sessionInfo} />}></Route>
+          <Route
+            path='/memberinfo'
+            element={<Mypage user={user} sessionInfo={sessionInfo} />}
+          ></Route>
+          <Route
+            path='/mypageupdate'
+            element={
+              <Mypage_updatemyinfo user={user} sessionInfo={sessionInfo} />
+            }
+          ></Route>
         </Routes>
-
-        
-
       </div>
     </>
   );
---
+++ store.js
@@ -0,0 +1,37 @@
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import userDataReducer from './reducers/userData';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+};
+const rootReducer = combineReducers({
+  userData: userDataReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
+export default store;
