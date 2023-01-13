48b1591943bf137b45d09a0dfcdd436c9bb1b169
Signalzero <chlehdnjs@gmail.com>
2023-01-13 09:37:54 +0900

    refactor: remove message input form to direct page

--- src/components/DM/ChatBox/ChatBox.jsx
+++ src/components/DM/ChatBox/ChatBox.jsx
@@ -1,45 +1,15 @@
 import React, { useEffect, useRef, useState } from 'react';
 import Message from '../Message/Message';
-import { serverTimestamp } from 'firebase/firestore';
 import styles from './ChatBox.module.css';
 import { useSelector } from 'react-redux';
 
-const ChatBox = ({ sendMessage, messageRepository }) => {
-  const userId = useSelector((state) => state.userData.catdogtimes_userId);
-  const displayName = useSelector(
-    (state) => state.userData.catdogtimes_displayName
-  );
-  const roomId = useSelector((state) => state.userData.catdogtimes_roomId);
-  const inRoom = useSelector((state) => state.userData.catdogtimes_inRoom);
-  const photoURL = useSelector((state) => state.userData.catdogtimes_photoURL);
-
-  const messageRef = useRef();
-  const formRef = useRef();
-  const scrollRef = useRef();
-
+const ChatBox = ({ messageRepository }) => {
   const [messages, setMessages] = useState({});
+  const inRoom = useSelector((state) => state.userData.catdogtimes_inRoom);
+  const roomId = useSelector((state) => state.userData.catdogtimes_roomId);
+  const userId = useSelector((state) => state.userData.catdogtimes_userId);
 
-  const onSubmit = (event) => {
-    event.preventDefault();
-    const message = {
-      userId: 'Owon',
-      roomId: roomId,
-      content: messageRef.current.value,
-      time: serverTimestamp(),
-      displayName: 'Dev_Owon',
-      photoURL: '/img/dog1.jpg',
-    };
-    messageRepository.saveMessage(message);
-    formRef.current.reset();
-  };
-  const onKeyPress = (event) => {
-    if (event.key === 'Enter') {
-      if (messageRef.current.value.trim() == '') {
-        return event.preventDefault();
-      }
-      onSubmit(event);
-    }
-  };
+  const scrollRef = useRef();
 
   useEffect(() => {
     scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
@@ -54,28 +24,16 @@ const ChatBox = ({ sendMessage, messageRepository }) => {
       setMessages(docs);
     });
     return () => stopSync();
-  }, [inRoom, messageRepository]);
+  }, [inRoom, roomId, messageRepository]);
 
   return (
-    <section className={styles.chatBox}>
+    <section className={styles.chatBox} ref={scrollRef}>
       <div className={styles.container}>
-        <ul className={styles.messages} ref={scrollRef}>
+        <ul className={styles.messages}>
           {Object.keys(messages).map((key) => (
             <Message key={key} message={messages[key]} userName={userId} />
           ))}
         </ul>
-        <div className={styles.messageInput}>
-          <div className={styles.inputMessage}>
-            <form className={styles.inputForm} ref={formRef} action=''>
-              <textarea
-                className={styles.textArea}
-                ref={messageRef}
-                row='4'
-                onKeyPress={onKeyPress}
-              ></textarea>
-            </form>
-          </div>
-        </div>
       </div>
     </section>
   );
