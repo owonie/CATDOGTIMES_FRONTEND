90114325b60b50d916fc84db9c897e9121e99f4a
SHINPC\jin <shinkookjin2@gmail.com>
2023-01-16 15:37:17 +0900

    동물 등록 확인

---
+++ src/routes/Mypage/MembershipWithdrawal.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect } from 'react';
+import Header from '../../components/mypage/Header';
+import Side from '../../components/mypage/Side';
+import Mobilemenu from '../../components/mypage/Mobilemenu';
+import Backtobtn from '../../components/mypage/Backtobtn';
+import Footer from '../../components/mypage/Footer';
+import Searchbox from '../../components/mypage/Searchbox';
+import Mywalks from '../../components/mypage/Mywalks';
+import MainHeader from '../../components/mypage/MainHeader';
+import TabSection from '../../components/mypage/TabSection';
+import JoinedWalks from '../../components/mypage/JoinedWalks';
+import { useSelector, useDispatch } from "react-redux";    
+
+import "antd/dist/antd";
+// import { DatePicker } from 'antd';
+
+const Mypage = (props) => {
+
+    const memberInfo = useSelector((state) => {
+        return state.memberInfo.data;
+    });
+
+return (
+<>
+
+    <div id="nt_wrapper">
+    <Header/>
+      <div id="nt_content" className="mainContent p-5">
+        <div>회원 탈퇴 신청</div>
+        
+        </div>
+        <Side  users={memberInfo}/>
+        <Footer/>
+    </div>
+    <Searchbox/>
+    <Mobilemenu/>
+    <Backtobtn/>
+</>
+);
+};
+
+export default Mypage;
