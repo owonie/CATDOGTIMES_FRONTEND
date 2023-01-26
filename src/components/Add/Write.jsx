87446398590870feb7823a1faf5cac75398b2e77
정재열(RE-Heat) <whitewolf1540@gmail.com>
2023-01-26 10:35:52 +0900

    주요작업(?) : 홑따옴표 -> 쌍따옴표 메인에서 pull 당기기전

--- src/components/Add/Write.jsx
+++ src/components/Add/Write.jsx
@@ -1,31 +1,27 @@
-import React, { useState } from 'react';
-import { Button, Input, Modal } from 'antd';
-import { BorderlessTableOutlined } from '@ant-design/icons';
-import styles from './Write.module.css';
-import UploadPicture from './UploadPicture';
-import TextArea from 'antd/es/input/TextArea';
-import { useSelector } from 'react-redux';
+import React, { useState } from "react";
+import { Button, Input, Modal } from "antd";
+import { BorderlessTableOutlined } from "@ant-design/icons";
+import styles from "./Write.module.css";
+import UploadPicture from "./UploadPicture";
+import TextArea from "antd/es/input/TextArea";
+import { useSelector } from "react-redux";
 
 const Write = () => {
   const [loading, setLoading] = useState(false);
   const [open, setOpen] = useState(false);
 
   /* 토큰 */
-  const accessToken = useSelector(
-    (state) => state.userData.catdogtimes_accessToken
-  );
-  const refreshToken = useSelector(
-    (state) => state.userData.catdogtimes_refreshToken
-  );
+  const accessToken = useSelector((state) => state.userData.catdogtimes_accessToken);
+  const refreshToken = useSelector((state) => state.userData.catdogtimes_refreshToken);
 
   /* postContent 저장용 */
-  const [postContent, setPostContent] = useState('');
+  const [postContent, setPostContent] = useState("");
   /* hashTag 저장용 */
-  const [postHashtag, setPostHashtag] = useState('');
+  const [postHashtag, setPostHashtag] = useState("");
   /* upload 저장용 */
-  const [imageOriginalName, setOriginalName] = useState('');
-  const [imageSavedName, setSavedName] = useState('');
-  const [file, setFile] = useState('');
+  const [imageOriginalName, setOriginalName] = useState("");
+  const [imageSavedName, setSavedName] = useState("");
+  const [file, setFile] = useState("");
 
   const setName = (name) => {
     console.log(imageSavedName);
@@ -42,11 +38,8 @@ const Write = () => {
   };
 
   const formData = new FormData();
-  formData.append(
-    'post',
-    new Blob([JSON.stringify(post)], { type: 'application/json' })
-  );
-  formData.append('file', file);
+  formData.append("post", new Blob([JSON.stringify(post)], { type: "application/json" }));
+  formData.append("file", file);
 
   const showModal = () => {
     setOpen(true);
@@ -54,7 +47,7 @@ const Write = () => {
 
   const addPost = async () => {
     const response = await fetch(`post/add`, {
-      method: 'POST',
+      method: "POST",
       headers: {
         ACCESS_TOKEN: accessToken,
       },
@@ -65,7 +58,7 @@ const Write = () => {
 
     if (response.status === 401) {
       const res = await fetch(`post/add`, {
-        method: 'POST',
+        method: "POST",
         headers: {
           ACCESS_TOKEN: accessToken,
           REFRESH_TOKEN: refreshToken,
@@ -92,31 +85,26 @@ const Write = () => {
 
   return (
     <>
-      <a href='#' onClick={showModal}>
-        <i className='fa-solid fa-pen-nib fa-lg'></i>
+      <a href="#" onClick={showModal}>
+        <i className="fa-solid fa-pen-nib fa-lg"></i>
         <span>글쓰기</span>
       </a>
       <Modal
         open={open}
-        title='새 게시물 올리기'
+        title="새 게시물 올리기"
         onOk={handleOk}
         onCancel={handleCancel}
         bodyStyle={{ height: 400 }}
         footer={[
-          <Button key='back' onClick={handleCancel}>
+          <Button key="back" onClick={handleCancel}>
             Cancel
           </Button>,
-          <Button
-            key='submit'
-            type='primary'
-            loading={loading}
-            onClick={handleOk}
-          >
+          <Button key="submit" type="primary" loading={loading} onClick={handleOk}>
             Submit
           </Button>,
         ]}
       >
-        <div id='upload' className={styles.write__upload}>
+        <div id="upload" className={styles.write__upload}>
           <UploadPicture setName={setName} bodyStyle={{ height: 400 }} />
           <div className={styles.write__text}>
             <TextArea
@@ -125,22 +113,17 @@ const Write = () => {
               style={{
                 height: 200,
               }}
-              size='large'
+              size="large"
               onChange={(e) => {
                 setPostContent(e.target.value);
               }}
-              placeholder='오늘은 어땠나요?'
+              placeholder="오늘은 어땠나요?"
               allowClear
               value={postContent}
             />
             <Input
-              placeholder='Enter hashtag'
-              prefix={
-                <BorderlessTableOutlined
-                  type='hashtag'
-                  style={{ color: 'rgba(0,0,0,.25)' }}
-                />
-              }
+              placeholder="Enter hashtag"
+              prefix={<BorderlessTableOutlined type="hashtag" style={{ color: "rgba(0,0,0,.25)" }} />}
               onChange={(e) => {
                 setPostHashtag(e.target.value);
                 console.log(e.target.value);
--- src/components/mypage/Tab03.jsx
+++ src/components/mypage/Tab03.jsx
@@ -1,31 +1,28 @@
-import React from 'react';
+import React from "react";
 
 const Tab03 = ({ data }) => {
   console.log(data);
-  const imgPath = 'http://localhost:8088/times/resources/upload/';
+  const imgPath = "http://localhost:8088/times/resources/upload/";
   return (
     <>
       {/* <h1>북마크</h1> */}
-      <ul className='bookmarklist'>
+      <ul className="bookmarklist">
         {data !== null ? (
           data.map((da, i) => (
             <li key={i}>
-              <a href='#'>
-                <span className='no'> {i + 1} </span>
-                <span className='thum'>
-                  <img
-                    src={`${imgPath}${da.imageSavedName}`}
-                    alt='{da.postContent}'
-                  />
+              <a href="#">
+                <span className="no"> {i + 1} </span>
+                <span className="thum">
+                  <img src={`${imgPath}${da.imageSavedName}`} alt="{da.postContent}" />
                 </span>
-                <span className='flex column ptxt'>
-                  <span className='ptitle'> {da.postContent} </span>
-                  <span className='writer'>@{da.memberNickname}</span>
-                  <span className='cnt'>
-                    {' '}
+                <span className="flex column ptxt">
+                  <span className="ptitle"> {da.postContent} </span>
+                  <span className="writer">@{da.memberNickname}</span>
+                  <span className="cnt">
+                    {" "}
                     좋아요( {da.likeCnt} ) 댓글( {da.replyCnt} )
                   </span>
-                  <span className='lastUpdate'>{da.postUpdateDate}</span>
+                  <span className="lastUpdate">{da.postUpdateDate}</span>
                 </span>
               </a>
             </li>
